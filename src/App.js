be00e0d93d62114d7e94c3e2fa31a886feeae5da
Minchul Kim <minchul.kim@snu.ac.kr>
2019-11-30 20:01:08 +0900

    adding items and hover effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Homepage from './pages/homepage/homepage.component'
 import './App.css';
 
 function App() {
   return (
     <div>
-      hello
+      <Homepage/>
     </div>
   );
 }
